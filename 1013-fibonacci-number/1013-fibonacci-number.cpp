de3cb655aacc2352de80dbbec7a5c2a2c1ac294a
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2025-08-11 00:09:47 -0500

    Time: 0 ms (100%), Space: 7.6 MB (87.29%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int n1 = 0; 
+        int n2 = 1;
+        int sum = n1 + n2;
+        for (int i = 2; i < n; i++)
+        {
+            n1 = n2;
+            n2 = sum;
+            sum = n1 + n2;
+        }
 
-        return fib(n - 1) + fib(n - 2);
+        return sum;
     }
 };
