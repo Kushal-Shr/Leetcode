1f9b9761be0dbedd810f5ce3b526d604a648052a
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2025-08-21 21:19:40 -0500

    Time: 4 ms (11.72%), Space: 16.8 MB (6.3%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    void solve(vector<int> nums, vector<int> output, int idx, vector<vector<int>>& ans)
+    {
+        if (idx >= nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+        // include 
+        solve(nums, output, idx + 1, ans);
+        
+        // exclude
+        int element = nums[idx];
+        output.push_back(element);
+        solve(nums, output, idx + 1, ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int idx = 0;
+
+        solve(nums, output, idx, ans);
+
+        return ans;
+    }
+};
