b196f6149166fd007d167dfbd070af71a41c2680
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2025-09-27 20:43:57 -0500

    Time: 2 ms (51.63%), Space: 8.6 MB (40.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (x != 0)
+        {
+            reversed = reversed * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return (reversed == temp);
+    }
+};
