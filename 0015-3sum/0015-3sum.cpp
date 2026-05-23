357ede7bad20ff314b155ea6247ab1b98f60ab9c
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2026-05-23 01:12:04 -0500

    Time: 51 ms (43.69%), Space: 29 MB (71.83%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,38 +3,44 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
 
-        set<vector<int>> triplets;
+        // set<vector<int>> triplets;
+        vector<vector<int>> ans;
         vector<int> trip;
 
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n; i++)
         {
-            int j = i + 1, k = n - 1;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+                int j = i + 1, k = n - 1;
 
-            while (j < k)
-            {
-                if (nums[i] + nums[j] + nums[k] == 0)
+                while (j < k)
                 {
-                    trip = {nums[i], nums[j], nums[k]};
-                    triplets.insert(trip);
-                    k--;
-                    j++;
-                }
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                    {
+                        trip = {nums[i], nums[j], nums[k]};
+                        ans.push_back(trip);
+                        k--;
+                        j++;
 
-                else if (nums[i] + nums[j] + nums[k] > 0)
-                {
-                    k--;
-                }
+                        while (j < k && nums[j] == nums[j - 1])
+                            j++;
+                    }
 
-                else if (nums[i] + nums[j] + nums[k] < 0)
-                {
-                    j++;
+                    else if (nums[i] + nums[j] + nums[k] > 0)
+                    {
+                        k--;
+                    }
+
+                    else if (nums[i] + nums[j] + nums[k] < 0)
+                    {
+                        j++;
+                    }
                 }
-            }
         }
 
-        vector<vector<int>> ans(triplets.begin(), triplets.end());
+        // vector<vector<int>> ans(triplets.begin(), triplets.end());
         
         return ans;
     }
