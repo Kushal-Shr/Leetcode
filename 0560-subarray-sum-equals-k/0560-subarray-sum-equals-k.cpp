a9b8f0822721ba661ebc3591806e7f41daea2abc
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2026-05-23 11:33:52 -0500

    Time: 51 ms (41.41%), Space: 52.5 MB (6.43%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int i = 0;
-        int sumCount = 0;
         int n = nums.size();
+        vector<int> pSum(n, 0);
+        int count = 0;
 
-        while (i < n)
+        pSum[0] = nums[0];
+        for (int i = 1; i < n; i++)
         {
-            int sum = 0;
-
-            int j = i;
-
-            while (j < n)
-            {
-                sum += nums[j];
-
-                if (sum == k)
-                {
-                    sumCount++;
-                }
-
-                j++;
-            }
-
-            i++;
+            pSum[i] = pSum[i - 1] + nums[i];
         }
 
-        return sumCount;
+        unordered_map<int, int> psFreq;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (k == pSum[j]) count++;
+
+            if (psFreq[pSum[j] - k] > 0)
+            {
+                count += psFreq[pSum[j] - k];
+            }
+
+            psFreq[pSum[j]]++;
+        }
+
+        return count;
     }
 };
