d43822d63204f27e3dc0c9ac9754589497ce5d26
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2026-05-25 22:54:01 -0500

    Time: 2 ms (39.41%), Space: 10.4 MB (83.87%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    void subsets(vector<int> &nums, vector<vector<int>> &ans, vector<int> &subset, int i)
+    {
+        if (i == nums.size())
+        {
+            if (find(ans.begin(), ans.end(), subset) == ans.end())
+            {
+                ans.push_back(subset);
+                return;     
+            }
+
+            else
+                return;
+        }
+
+        subset.push_back(nums[i]);
+        subsets(nums, ans, subset, i + 1);
+
+        subset.pop_back();
+        subsets(nums, ans, subset, i + 1);
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> subset;
+
+        int i = 0;
+
+        subsets(nums, ans, subset, i);
+
+        return ans;
+    }
+};
