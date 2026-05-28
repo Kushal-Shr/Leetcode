e2c86b4744c9d9eae05b497069471435376f06c5
Kushal Shrestha <133557671+Kushal-Shr@users.noreply.github.com>
2026-05-28 12:28:19 -0500

    Time: 2 ms (36.31%), Space: 10.5 MB (87.79%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,34 @@
 class Solution {
-private:
-    void solve(vector<int> nums, int idx, vector<vector<int>>& ans)
+public:
+    void getPerms(vector<int> &nums, vector<vector<int>> &ans, vector<int> &perm, vector<bool> &used)
     {
-        if (idx >= nums.size())
+        int n = nums.size();
+
+        if (perm.size() == n)
         {
-            ans.push_back(nums);
+            ans.push_back(perm);
             return;
         }
 
-        for(int i = idx; i < nums.size(); i++)
+        for (int i = 0; i < n; i++)
         {
-            swap(nums[idx], nums[i]);
-            solve(nums, idx + 1, ans);
-            swap(nums[idx], nums[i]);
+            if (used[i]) continue;
+
+            perm.push_back(nums[i]);
+            used[i] = true;
+            getPerms(nums, ans, perm, used);
+
+            used[i] = false;
+            perm.pop_back();
         }
     }
-public:
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int idx = 0;
-
-        solve(nums, idx, ans);
-
+        vector<int> perm;
+        vector<bool> used(nums.size());
+        getPerms(nums, ans, perm, used);
+        
         return ans;
     }
 };
